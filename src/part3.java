dd6295d2b4f2bc1cfd773b0118a9b1abf0070429
Light-Center <pconstance106@mycr.redwoods.edu>
2022-10-04 13:29:14 -0700

    added part 3

---
+++ src/part3.java
@@ -0,0 +1,31 @@
+package src;
+
+class part3 {
+    /**
+     * PART 3 - Code Scanning and Interpretation
+     *      * Look at the getHiddenSecrets method and identify the following parts:
+     *
+     * What is the access modifier: (public, private, protected)?
+     * // The access modifier is public. --> "public" class HiddenSecrets
+     *
+     * Is it a Class method or an object Instance method, how do you know?
+     * // The method is a Class method. --> public "class" HiddenSecrets
+     *
+     * What is its return data-type?
+     * // There return data type is Void. --> public static "void" getHiddenSecrets(File file)
+     *
+     * Does it require any parameters, and if so, how many and of what type?
+     * // Void returns no value. The variable File is the return value for public static void getHiddenSecrets(File file)
+     *
+     *      *Scan line by line through the code and try to determine how it works?
+     * What is familiar to you?
+     * // public, class, import, new, if, err, catch, metadata
+     *
+     * What is not familiar to you?
+     * // for
+     *
+     * Do the for loops make sense, and if so, tell me what you think they do?
+     * //I understand it better with the error and catch as context that the for statements looks at the condition and returns either true or false. This is chapter 7.2
+     *
+     */
+}
